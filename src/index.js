8adbfeac65347b4aa8a75d9bbf98a469c22e926a
Akash1507 <atakashsharma@gmail.com>
2020-01-10 04:15:41 +0530

    add custom js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDom from "react-dom";
+import Chat from "./containers/Chat";
 
 class App extends React.Component {
   render() {
-    return <div>Hello</div>;
+    return <Chat />;
   }
 }
 
