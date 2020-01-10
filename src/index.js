4abbd7d856c6c895d5072bb7df103a6bb2def5f1
Akash1507 <atakashsharma@gmail.com>
2020-01-10 06:13:27 +0530

    add Web Socket connect method in component life cycle

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
 import Chat from "./containers/Chat";
-
+import WebSocketInstance from "./websockets";
 class App extends React.Component {
+  componentDidMount() {
+    WebSocketInstance.connect();
+  }
   render() {
     return <Chat />;
   }
